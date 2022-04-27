3e05730553b2ac66c350b691a27cc147156e290b
Dopamine <motor55005@gmail.com>
2022-04-27 19:43:17 +0300

    Реализует отрисовку основных элементов

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import UserRankView from './view/user-rank-view.js';
+import NavigationMenuView from './view/navigation-menu-view.js';
+import SortView from './view/sort-view.js';
+import FilmsPresenter from './presenter/films-presenter.js';
+import MoviesAmountView from './view/movies-amount-view.js';
+import { render } from './render.js';
+
+const siteHeaderElement = document.querySelector('.header');
+const siteMainElement = document.querySelector('.main');
+const siteFooterElement = document.querySelector('.footer');
+const filmsPresenter = new FilmsPresenter();
+
+render(new UserRankView(), siteHeaderElement);
+render(new NavigationMenuView(), siteMainElement);
+render(new SortView(), siteMainElement);
+
+filmsPresenter.init(siteMainElement);
+
+render(new MoviesAmountView(), siteFooterElement);
